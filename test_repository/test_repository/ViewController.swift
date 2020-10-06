572f02081d4279e6481a1341203bbe792927b018
nisimuraK <tomo-saki-kana@i.softbank.jp>
2020-10-06 16:05:01 +0900

    画面デザインなどの設定

---
+++ test_repository/test_repository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_repository
+//
+//  Created by 西村奏人 on 2020/10/06.
+//  Copyright © 2020 西村奏人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var answerlmageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func ShuffluAction(_ sender: Any) {
+    }
+}
+
