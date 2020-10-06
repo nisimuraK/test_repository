c2a425280bd16ad88318a0e38143008b14613138
nisimuraK <tomo-saki-kana@i.softbank.jp>
2020-10-07 00:41:32 +0900

    ジャンケンの表示と乱数を設定

--- test_repository/test_repository/ViewController.swift
+++ test_repository/test_repository/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,8 +18,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerlmageView: UIImageView!
     
     @IBOutlet weak var answerLabel: UILabel!
+    //ジャンケン(数字)
+    var answerNumber = 0
     
     @IBAction func ShuffluAction(_ sender: Any) {
+        //1〜3の乱数
+        answerNumber = Int.random(in:0..<3)
+        
+        if answerNumber == 0{
+        answerLabel.text = "グー"
+        answerlmageView.image = UIImage(named:"janken_gu")
+            
+        }else if answerNumber == 1{
+            answerLabel.text = "チョキ"
+            answerlmageView.image = UIImage(named:"janken_choki")
+            
+        }else if answerNumber == 2{
+            answerLabel.text = "パー"
+            answerlmageView.image = UIImage(named:"janken_pa")
+        }
+        //次の試合
+       // answerNumber = answerNumber + 1
     }
 }
 
